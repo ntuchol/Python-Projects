fd8a8143704bea5f7a82d4aa5a9936ddc589c8aa
ntuchol <ntuchol@bu.edu>
2016-10-01 21:56:15 -0500

    Add files via upload

---
+++ Mad Libs Generator.py
@@ -0,0 +1,7 @@
+exclamation = raw_input("Please choose an exclamation:")
+adverb = raw_input("Choose an adverb: ")
+noun = raw_input("Choose a noun: " )
+adjective = raw_input("Choose an adjective: ") 
+
+print exclamation + "! he said " + adverb + " as he jumped into his convertible "\
++ noun + " and drove off with his " + adjective + " wife." 
