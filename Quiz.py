a3436e7d278cd533f66c950b4c30e393b610fd4d
ntuchol <ntuchol@bu.edu>
2025-04-26 22:59:34 -0500

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,28 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+answer = input("What was the name of the crime boss who was head of the feared Chicago Outfit?")
+if answer == "Al Capone":
+    print("Correct!")
+else:
+    print(f"The answer is 'Al Capone', not {answer!r}")
+
+answer = input("What year was the United Nations established?")
+if answer == "1945":
+    print("Correct!")
+else:
+    print(f"The answer is '1945', not {answer!r}")
+answer = input("What Renaissance artist is buried in Rome's Pantheon?")
+if answer == "Raphael":
+    print("Correct!")
+else:
+    print(f"The answer is 'Raphael', not {answer!r}")
+answer = input("Who was the last Tsar of Russia?")
+if answer == "Nicholas II":
+    print("Correct!")
+else:
+    print(f"The answer is 'Nicholas II', not {answer!r}")
+answer = input("What Renaissance artist painted the Sistine Chapel ceiling?")
+if answer == "Michelangelo":
+    print("Correct!")
+else:
+    print(f"The answer is 'Michelangelo', not {answer!r}")
