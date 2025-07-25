fc5768633f20ecc9c7d4b47aca6adc630349df76
Natalia <ntuchol@bu.edu>
2025-07-25 17:17:59 -0500

    Update lynxtrappings.py

--- lynxtrappings.py
+++ lynxtrappings.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the dataset
 data = pd.read_csv('lynx.csv', index_col='year')
 
-# Plot the time series
 plt.figure(figsize=(10, 6))
 plt.plot(data['trappings'])
 plt.title('Canadian Lynx Trappings (1821-1934)')
