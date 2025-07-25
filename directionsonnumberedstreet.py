ea768c01415bfd433c57c54cea03f535a13e560e
Natalia <ntuchol@bu.edu>
2025-07-25 14:46:30 -0500

    Update directionsonnumberedstreet.py

--- directionsonnumberedstreet.py
+++ directionsonnumberedstreet.py
@@ -1,17 +1,13 @@
 import googlemaps
 
-# Initialize the Google Maps client with your API key
 API_KEY = "YOUR_GOOGLE_MAPS_API_KEY"
 gmaps = googlemaps.Client(key=API_KEY)
 
-# Define the origin and destination
 origin = "34th Street, New York, NY"
 destination = "42nd Street, New York, NY"
 
-# Request directions
 directions_result = gmaps.directions(origin, destination, mode="walking")
 
-# Parse and display the directions
 if directions_result:
     steps = directions_result[0]['legs'][0]['steps']
     print("Directions:")
