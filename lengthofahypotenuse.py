757e96886b4f39ad2ae1271f5462651d58eb6de7
Natalia <ntuchol@bu.edu>
2025-07-25 17:06:51 -0500

    Update lengthofahypotenuse.py

--- lengthofahypotenuse.py
+++ lengthofahypotenuse.py
@@ -1,9 +1,7 @@
 import math
 
-# Lengths of the two shorter sides
 a = 3
 b = 4
 
-# Calculate the hypotenuse
 hypotenuse = math.sqrt(a**2 + b**2)
 print(f"The length of the hypotenuse is {hypotenuse}")
