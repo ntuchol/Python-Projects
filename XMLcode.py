a73d518a997b31c5988557f8e80010b4a044b04c
Natalia <ntuchol@bu.edu>
2025-07-24 22:56:31 -0500

    Update XMLcode.py

--- XMLcode.py
+++ XMLcode.py
@@ -1,10 +1,8 @@
 import xml.etree.ElementTree as ET
 
-# Parse the XML file
 tree = ET.parse('data.xml')
 root = tree.getroot()
 
-# Iterate through the XML structure
 for book in root.findall('book'):
     title = book.find('title').text
     author = book.find('author').text
