165cab11d5cb8dff700fba21b4160c2a068f5603
Natalia <ntuchol@bu.edu>
2025-07-25 09:43:26 -0500

    Update datetime.py

--- datetime.py
+++ datetime.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
    now = datetime.now()
-   print(now) # Output: 2025-06-07 11:13:00.123456 (example)
-   print(now.year)   # Output: 2025
-   print(now.month)  # Output: 6
-   print(now.day)    # Output: 7
-   print(now.hour)   # Output: 11
-   print(now.minute) # Output: 13
+   print(now) 
+   print(now.year)  
+   print(now.month)  
+   print(now.day)    
+   print(now.hour)   
+   print(now.minute)
