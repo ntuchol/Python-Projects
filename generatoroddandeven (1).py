eb0872c45b399cd00018de3550b8964706bed92f
Natalia <ntuchol@bu.edu>
2025-07-25 15:24:09 -0500

    Update generatoroddandeven (1).py

--- generatoroddandeven (1).py
+++ generatoroddandeven (1).py
@@ -1,12 +1,10 @@
 def even_odd_generator():
-    """
-    A generator that yields alternating even and odd numbers.
-    """
+    
     current_number = 0
     while True:
-        if current_number % 2 == 0:  # Even number
+        if current_number % 2 == 0:  
             yield current_number
             current_number += 1
-        else:  # Odd number
+        else:  
             yield current_number
             current_number += 1
