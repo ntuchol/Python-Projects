be9d7630d8950c0e949ec645ef0c2d791148503b
Natalia <ntuchol@bu.edu>
2025-07-24 21:39:32 -0500

    Update Gephi.py

--- Gephi.py
+++ Gephi.py
@@ -1,11 +1,9 @@
 import networkx as nx
     import pandas as pd
 
-    # Create a simple graph
     G = nx.Graph()
     G.add_edges_from([(1, 2), (1, 3), (2, 3), (3, 4)])
 
-    # Export nodes and edges to CSV
     nodes_df = pd.DataFrame(G.nodes(data=True), columns=['Id', 'Attributes'])
     edges_df = pd.DataFrame(G.edges(), columns=['Source', 'Target'])
 
