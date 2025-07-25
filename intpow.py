6063a908c9b8688f79dfa301ef8ddd6f619637d5
Natalia <ntuchol@bu.edu>
2025-07-25 16:32:23 -0500

    Update intpow.py

--- intpow.py
+++ intpow.py
@@ -1,17 +1,6 @@
 
 
 def intpow(base, exponent):
-    """
-    Calculates the power of an integer using recursion.
-
-    Args:
-        base: The base integer.
-        exponent: The exponent integer.
-
-    Returns:
-        The result of base raised to the power of exponent.
-    """
-
     if exponent < 0:
       return 1 / intpow(base, -exponent)
     if exponent == 0:
