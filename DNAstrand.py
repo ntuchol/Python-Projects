9e58760b882529a38d3fd406f551bd1e3237dae1
Natalia <ntuchol@bu.edu>
2025-07-24 21:24:43 -0500

    Update DNAstrand.py

--- DNAstrand.py
+++ DNAstrand.py
@@ -1,12 +1,7 @@
 def verify(sequence):
-    '''This code verifies if a sequence is a DNA or RNA'''
-    # set the input sequence
+    
     seq = set(sequence)
      
-    # confirm if its elements is equal to the
-    # set of valid DNA bases
-    # Use a union method to ensure the sequence is
-    # verified if does not contain all the bases
     if seq == {"A", "T", "C", "G"}.union(seq):
         return "DNA"
     elif seq == {"A", "U", "C", "G"}.union(seq):
@@ -24,16 +19,9 @@ print(seq2 + " is " + verify(seq2))
 print(seq3 + " is " + verify(seq3))
 
 def verify(sequence):
-    '''This code verifies if a sequence is a DNA or RNA'''
-     
-    # set the input sequence
+    
     seq = set(sequence)
      
-    # confirm if its elements is equal to 
-    # the set of valid DNA bases
-    # Use a union method to ensure the
-    # sequence is verified if does not
-    # contain all the bases
     if seq == {"A", "T", "C", "G"}.union(seq):
         return "DNA"
     elif seq == {"A", "U", "C", "G"}.union(seq):
@@ -43,35 +31,28 @@ def verify(sequence):
  
  
 def rev_comp_st(seq):
-    '''This function returns a reverse complement 
-    of a DNA or RNA strand'''
     verified = verify(seq)
     if verified == "DNA":
        
-        # complement strand
         seq = seq.replace("A", "t").replace(
             "C", "g").replace("T", "a").replace("G", "c")
         seq = seq.upper()
          
-        # reverse strand
         seq = seq[::-1]
         return seq
  
     elif verified == "RNA":
        
-        # complement strand
         seq = seq.replace("A", "u").replace(
             "C", "g").replace("U", "a").replace("G", "c")
         seq = seq.upper()
          
-        # reverse strand
         seq = seq[::-1]
         return seq
     else:
         return "Invalid sequence"
  
  
-# test variables
 seq1 = "ATGCAGCTGTGTTACGCGAT"
 seq2 = "UGGCGGAUAAGCGCA"
 seq3 = "TYHGGHHHHH"
