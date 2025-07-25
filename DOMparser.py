60883bba52419c01075a26a4b67a0f233a9334bc
Natalia <ntuchol@bu.edu>
2025-07-24 21:26:06 -0500

    Update DOMparser.py

--- DOMparser.py
+++ DOMparser.py
@@ -1,31 +1,18 @@
 from xml.dom.minidom import parse, parseString
 
-# Assuming you have an XML file named 'config.xml' with configuration information
-# For example:
-# <configuration>
-#   <setting name="database_url">localhost:5432</setting>
-#   <setting name="username">admin</setting>
-#   <setting name="password">secret</password>
-# </configuration>
-
 try:
-    # Parse the XML file
     dom = parse('config.xml')
 
-    # Get the root element
     root = dom.documentElement
 
-    # Get all the 'setting' elements
     settings = root.getElementsByTagName('setting')
 
-    # Extract configuration information
     config = {}
     for setting in settings:
         name = setting.getAttribute('name')
         value = setting.firstChild.nodeValue.strip()
         config[name] = value
 
-    # Print the extracted configuration
     print(config)
 
 except Exception as e:
