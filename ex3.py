7a12807dbf05d00cfca5a755e462ab433158dde8
Natalia <ntuchol@bu.edu>
2025-07-25 14:56:17 -0500

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,12 +1,7 @@
-#This line prints I will now count my chickens to the console
 print("I will now count my chickens:")
-#This line prints "Hens" and the result of adding (30/6) = 5 to 25 (This result is 30)
 print("Hens", 25+30/6)
-#This line prints "Roosters" and the result of the equation given below- which is 60 
 print("Roosters", 100 - 25 * 3 % 4)
-#Prints I will now count the eggs:- to the console 
 print("Now I will count the eggs:")
-#etc.., just printing results of equations to the console as well as statements 
 print(3 + 2 + 1 - 5 + 4 % 2 - 1/(4+6))
 print("Is it true that 3 + 2 < 5 - 7?")
 print(3 + 2 < 5 -7)
