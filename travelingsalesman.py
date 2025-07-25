8cdb122ddfdb25ae8c5546be71e9abd943e2f371
Natalia <ntuchol@bu.edu>
2025-07-24 20:13:52 -0500

    Update travelingsalesman.py

--- travelingsalesman.py
+++ travelingsalesman.py
@@ -1,24 +1,13 @@
-# Python3 implementation of the above approach
 from random import randint
 
 INT_MAX = 2147483647
-# Number of cities in TSP
 V = 5
 
-# Names of the cities
 GENES = "ABCDE"
 
-# Starting Node Value
 START = 0
 
-# Initial population size for the algorithm
 POP_SIZE = 10
-
-# Structure of a GNOME
-# defines the path traversed
-# by the salesman while the fitness value
-# of the path is stored in an integer
-
 
 class individual:
     def __init__(self) -> None:
@@ -31,15 +20,9 @@ class individual:
     def __gt__(self, other):
         return self.fitness > other.fitness
 
-
-# Function to return a random number
-# from start and end
 def rand_num(start, end):
     return randint(start, end-1)
 
-
-# Function to check if the character
-# has already occurred in the string
 def repeat(s, ch):
     for i in range(len(s)):
         if s[i] == ch:
@@ -47,11 +30,6 @@ def repeat(s, ch):
 
     return False
 
-
-# Function to return a mutated GNOME
-# Mutated GNOME is a string
-# with a random interchange
-# of two genes to create variation in species
 def mutatedGene(gnome):
     gnome = list(gnome)
     while True:
@@ -64,9 +42,6 @@ def mutatedGene(gnome):
             break
     return ''.join(gnome)
 
-
-# Function to return a valid GNOME string
-# required to create the population
 def create_gnome():
     gnome = "0"
     while True:
@@ -80,10 +55,6 @@ def create_gnome():
 
     return gnome
 
-
-# Function to return the fitness value of a gnome.
-# The fitness value is the path length
-# of the path represented by the GNOME.
 def cal_fitness(gnome):
     mp = [
         [0, 2, INT_MAX, 12, 5],
@@ -100,31 +71,16 @@ def cal_fitness(gnome):
 
     return f
 
-
-# Function to return the updated value
-# of the cooling element.
 def cooldown(temp):
     return (90 * temp) / 100
 
-
-# Comparator for GNOME struct.
-# def lessthan(individual t1,
-#               individual t2)
-# :
-#     return t1.fitness < t2.fitness
-
-
-# Utility function for TSP problem.
 def TSPUtil(mp):
-    # Generation Number
     gen = 1
-    # Number of Gene Iterations
     gen_thres = 5
 
     population = []
     temp = individual()
 
-    # Populating the GNOME pool.
     for i in range(POP_SIZE):
         temp.gnome = create_gnome()
         temp.fitness = cal_fitness(temp.gnome)
@@ -138,8 +94,6 @@ def TSPUtil(mp):
     found = False
     temperature = 10000
 
-    # Iteration to perform
-    # population crossing and gene mutation.
     while temperature > 1000 and gen <= gen_thres:
         population.sort()
         print("\nCurrent temp: ", temperature)
@@ -160,8 +114,6 @@ def TSPUtil(mp):
 
                 else:
 
-                    # Accepting the rejected children at
-                    # a possible probability above threshold.
                     prob = pow(
                         2.7,
                         -1
