1c944330ac63b7c9bbfae2240c97d046c777ca23
Natalia <ntuchol@bu.edu>
2025-07-24 22:36:51 -0500

    Update SLR_Model.py

--- SLR_Model.py
+++ SLR_Model.py
@@ -2,8 +2,8 @@ import numpy as np
     from sklearn.linear_model import LinearRegression
     from sklearn.model_selection import train_test_split
     from sklearn.metrics import mean_squared_error, r2_score
-    X = np.array([1, 2, 3, 4, 5]).reshape(-1, 1) # Independent variable
-    y = np.array([2, 4, 5, 4, 5])              # Dependent variable
+    X = np.array([1, 2, 3, 4, 5]).reshape(-1, 1) 
+    y = np.array([2, 4, 5, 4, 5])            
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model = LinearRegression()
     model.fit(X_train, y_train)
