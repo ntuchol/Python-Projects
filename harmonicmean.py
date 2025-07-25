6ee8a3d4c21a9b501b919009c522c6a80f9f39e4
Natalia <ntuchol@bu.edu>
2025-07-25 16:00:18 -0500

    Update harmonicmean.py

--- harmonicmean.py
+++ harmonicmean.py
@@ -3,16 +3,13 @@ from scipy.stats import hmean
 
 data = [1, 2, 3, 4, 5]
 
-# Using statistics.harmonic_mean()
 harmonic_mean_1 = statistics.harmonic_mean(data)
 print(f"Harmonic mean using statistics: {harmonic_mean_1}")
 
-# Using scipy.stats.hmean()
 harmonic_mean_2 = hmean(data)
 print(f"Harmonic mean using scipy: {harmonic_mean_2}")
 
 
-#Example with weights using scipy.stats.hmean()
 data2 = [1, 4, 7]
 weights = [3, 1, 3]
 harmonic_mean_3 = hmean(data2, weights=weights)
