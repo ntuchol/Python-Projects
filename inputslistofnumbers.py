d1c38439edbcd27d1e1093317e8d9631cc0ab906
Natalia <ntuchol@bu.edu>
2025-07-25 16:24:16 -0500

    Update inputslistofnumbers.py

--- inputslistofnumbers.py
+++ inputslistofnumbers.py
@@ -1,12 +1,7 @@
-# Program to input a list of numbers and perform basic operations
-
-# Input: Prompt the user to enter numbers separated by spaces
 numbers = input("Enter a list of numbers separated by spaces: ")
 
-# Convert the input string into a list of integers
 l = list(map(int, numbers.split()))
 
-# Output the list and perform basic operations
 print("\nYou entered the list:", l)
 print("Sum of the numbers:", sum(l))
 print("Maximum number:", max(l))
