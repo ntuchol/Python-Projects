3e26eb29560e7ed7e102c4dd68fd7f9da854bf83
Natalia <ntuchol@bu.edu>
2025-07-25 16:03:14 -0500

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -2,11 +2,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Create a sample 2D dataset
 data = np.random.rand(10, 10)
 
-# Create the heatmap
 sns.heatmap(data)
 
-# Display the plot
 plt.show()
