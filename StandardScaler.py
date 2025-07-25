8553057f569efe4144af80a1ee54eb343c5356a3
Natalia <ntuchol@bu.edu>
2025-07-24 22:46:18 -0500

    Update StandardScaler.py

--- StandardScaler.py
+++ StandardScaler.py
@@ -1,16 +1,13 @@
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
-# Sample data
 X = np.array([[10, 1],
               [20, 2],
               [30, 3],
               [40, 4]])
 
-# Create a StandardScaler object
 scaler = StandardScaler()
 
-# Fit the scaler to the data and transform it
 scaled_data = scaler.fit_transform(X)
 
 print("Original Data:\n", X)
