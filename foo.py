f95b162e383185450856d33efae12a1a969c6c66
Natalia <ntuchol@bu.edu>
2025-07-25 15:12:34 -0500

    Update foo.py

--- foo.py
+++ foo.py
@@ -2,14 +2,13 @@ while True:
   user_input = input("Enter something: ")
   if user_input == "foo":
     print("You entered 'foo'! Now I'm printing this message.")
-    break # Exit the loop after the event
+    break 
 
 text_data = "This is some text with foo inside it."
 if "foo" in text_data:
   print("Found 'foo' in the text! Printing this message.")
   
 event_triggered = False
-# ... some code where an event might happen ...
 
 if "foo" in some_input_or_data:
   event_triggered = True
