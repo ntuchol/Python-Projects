0de1463e0d67f55d708ad4b3e1f8d243bf665906
Natalia <ntuchol@bu.edu>
2025-07-25 17:16:50 -0500

    Update and rename lookaheadfeature (1).py to look_ahead_feature.py

--- look_ahead_feature.py
+++ look_ahead_feature.py
@@ -1,8 +1,7 @@
 import re
 
-# Match 'foo' only if it is followed by 'bar' and NOT followed by 'baz'
 pattern = r'foo(?=bar)(?!baz)'
 text = "foobar foobaz foobarqux"
 
 matches = re.findall(pattern, text)
-print(matches)  # Output: ['foo']
+print(matches)  
