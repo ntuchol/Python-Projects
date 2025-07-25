6053873894640082b189c9e887bbd4768e5f019c
Natalia <ntuchol@bu.edu>
2025-07-25 16:18:36 -0500

    Update imfeelinglucky.py

--- imfeelinglucky.py
+++ imfeelinglucky.py
@@ -1,14 +1,12 @@
 
 import webbrowser, bs4, requests, sys
  
-print('Googling...') # display text while downloading the Google page
+print('Googling...') 
 res = requests.get('http://google.com/search?source=' + ' '.join(sys.argv[1:]))
 print(res.raise_for_status())
  
-# Retreive top search result links.
 soup = bs4.BeautifulSoup(res.text, "html.parser")
  
-# Open a browser tab for each result
 linkElems = soup.select('.r a')
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
