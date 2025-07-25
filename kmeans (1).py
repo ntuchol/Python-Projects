76e321f634550eba1004d2ac16e0d6e6aa7affad
Natalia <ntuchol@bu.edu>
2025-07-25 16:46:34 -0500

    Update kmeans (1).py

--- kmeans (1).py
+++ kmeans (1).py
@@ -1,19 +1,14 @@
 from sklearn.cluster import KMeans
 import numpy as np
 
-# Sample data
 X = np.array([[1, 2], [1.5, 1.8], [5, 8], [8, 8], [1, 0.6], [9, 11]])
 
-# Initialize KMeans with the desired number of clusters (k)
 kmeans = KMeans(n_clusters=2, random_state=0, n_init='auto')
 
-# Fit the model to the data
 kmeans.fit(X)
 
-# Get the cluster assignments for each data point
 labels = kmeans.labels_
 
-# Get the coordinates of the cluster centroids
 centroids = kmeans.cluster_centers_
 
 print("Cluster Labels:", labels)
