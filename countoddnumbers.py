8411b7d3ad61e1b865514b6ae0a3954bfdc4bc0b
Natalia <ntuchol@bu.edu>
2025-07-25 09:35:15 -0500

    Update countoddnumbers.py

--- countoddnumbers.py
+++ countoddnumbers.py
@@ -7,4 +7,4 @@ def count_odd_numbers_loop(numbers):
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 odd_count = count_odd_numbers_loop(numbers)
-print(f"Number of odd numbers: {odd_count}") # Output: Number of odd numbers: 5
+print(f"Number of odd numbers: {odd_count}") 
