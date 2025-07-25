8263d4049822fb3d890a400bf778c5f905b28f50
Natalia <ntuchol@bu.edu>
2025-07-24 19:31:40 -0500

    Simple_Calculator.py

--- A Simple Calculator.py
+++ A Simple Calculator.py
@@ -1,18 +1,13 @@
-#A Simple Calculator Program with 4 functions 
-
-#This function adds two numbers 
+ 
 def add(x, y):
    return x + y
 
-# This function subtracts two numbers 
 def subtract(x, y):
    return x - y
 
-# This function multiplies two numbers
 def multiply(x, y):
    return x * y
 
-# This function divides two numbers
 def divide(x, y):
    return x / y
 
@@ -22,7 +17,6 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-# Take input from the user 
 choice = int(input("Enter choice(1/2/3/4):"))
 
 num1 = int(input("Enter first number: "))
