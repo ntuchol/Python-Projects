4e9bc4ee3d9ed33f8f15e630d716c0e53551d881
Natalia <ntuchol@bu.edu>
2025-07-25 14:41:55 -0500

    Update determinantofamatrix.py

--- determinantofamatrix.py
+++ determinantofamatrix.py
@@ -1,10 +1,7 @@
 import numpy as np
 
-# Define a square matrix as a NumPy array
 matrix = np.array([[1, 2], [3, 4]])
 
-# Calculate the determinant using numpy.linalg.det()
 determinant = np.linalg.det(matrix)
 
-# Print the determinant
 print("Determinant:", determinant)
