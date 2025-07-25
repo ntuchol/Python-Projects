8fa7a42f582a8729d631e9ff030bdb8a7467e522
Natalia <ntuchol@bu.edu>
2025-07-24 20:10:00 -0500

    Update triangleofagivenside.py

--- triangleofagivenside.py
+++ triangleofagivenside.py
@@ -1,5 +1,5 @@
 def get_triangle(a,b,c):
   z = np.array([a,b,c])
   while z[-1] != z.max():
-    z = z[[2,0,1]] # make sure last entry is largest
+    z = z[[2,0,1]] 
   alpha, beta, _ = calc_angles(*z)
