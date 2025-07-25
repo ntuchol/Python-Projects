4cced9a43a527b7f54c9afe6d16e45492ed503da
Natalia <ntuchol@bu.edu>
2025-07-25 14:37:27 -0500

    Update decimalmodule.py

--- decimalmodule.py
+++ decimalmodule.py
@@ -1,13 +1,10 @@
 import decimal
 from decimal import Decimal
 
-# Setting precision
 decimal.getcontext().prec = 6
 
-# Creating Decimal instances
 a = Decimal('1.1')
 b = Decimal('2.2')
 
-# Performing arithmetic operations
 result = a + b
 print(result) # Output: 3.3
