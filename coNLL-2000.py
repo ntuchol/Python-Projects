a9c61ff8f002ec1cdf1f1c9052500493835f5c2b
Natalia <ntuchol@bu.edu>
2025-07-24 23:28:19 -0500

    Update coNLL-2000.py

--- coNLL-2000.py
+++ coNLL-2000.py
@@ -1,17 +1,13 @@
 import nltk
 from nltk.corpus import conll2000
 
-# Load the training set
 train_sents = conll2000.chunked_sents('train.txt')
 
-# Print the first chunked sentence
 print(train_sents[0])
 
-# Load only NP chunks
 np_train_sents = conll2000.chunked_sents('train.txt', chunk_types=['NP'])
 print(np_train_sents[0])
 
-# Example of converting to IOB format
 from nltk.chunk import conlltags2tree, tree2conlltags
 iob_tuples = tree2conlltags(train_sents[0])
 print(iob_tuples)
