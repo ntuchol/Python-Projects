ba24644ec01c2414e16bf58037669339ed57fb51
Natalia <ntuchol@bu.edu>
2025-07-25 14:52:45 -0500

    Update eigenvalues.py

--- eigenvalues.py
+++ eigenvalues.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-# Define a square matrix
 A = np.array([[4, -2],
               [1,  1]])
 
-# Calculate eigenvalues and eigenvectors
 eigenvalues, eigenvectors = np.linalg.eig(A)
 
 print("Eigenvalues:", eigenvalues)
@@ -12,7 +10,6 @@ print("Eigenvectors:\n", eigenvectors)
 A = np.array([[4, -2],
               [1,  1]])
 
-# Calculate only eigenvalues
 eigenvalues = np.linalg.eigvals(A)
 
 print("Eigenvalues:", eigenvalues)
