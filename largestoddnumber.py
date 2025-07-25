458a825bf61841434767062fad648558aa98e87e
Natalia <ntuchol@bu.edu>
2025-07-25 16:57:26 -0500

    Update largestoddnumber.py

--- largestoddnumber.py
+++ largestoddnumber.py
@@ -1,13 +1,4 @@
 def find_largest_odd(numbers):
-    """
-    Finds the largest odd number in a list of integers.
-
-    Args:
-      numbers: A list of integers.
-
-    Returns:
-      The largest odd number in the list, or -1 if no odd number is found.
-    """
     largest_odd = -1
     for number in numbers:
         if number % 2 != 0 and number > largest_odd:
