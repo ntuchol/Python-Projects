ee4af210f99976492ef810a752905d3e51a5f0a9
Natalia <ntuchol@bu.edu>
2025-07-25 16:58:56 -0500

    Update leibniz.py

--- leibniz.py
+++ leibniz.py
@@ -1,6 +1,3 @@
-# Calculation of PI using Leibniz formula
-
-# Function to calcultae PI using Leibniz
 def leibniz(n):
     t_sum = 0
     for i in range(n):
@@ -9,11 +6,6 @@ def leibniz(n):
     
     return t_sum * 4
 
-# Reading number of terms to be considered in Leibniz formula
 terms = int(input("Enter number of terms: "))
-
-# Function call
 pi = leibniz(terms)
-
-# Displaying result
 print("Pi = ", pi)
