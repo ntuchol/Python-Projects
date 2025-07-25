847e8b8869c74283b4c5933fb1f682e247005f5f
Natalia <ntuchol@bu.edu>
2025-07-24 19:59:46 -0500

    Update violin_plots.py

--- violin_plots.py
+++ violin_plots.py
@@ -1,23 +1,17 @@
 import seaborn as sns
     import matplotlib.pyplot as plt
 
-    # Load example dataset
     tips = sns.load_dataset("tips")
 
-    # Create a simple violin plot
     sns.violinplot(x="day", y="total_bill", data=tips)
     plt.show()
-    # Generate some sample data
     data = [np.random.normal(0, std, 100) for std in range(1, 4)]
 
-    # Create a violin plot
     plt.violinplot(data)
     plt.show()
     import plotly.express as px
 
-    # Load example dataset
     df = px.data.tips()
 
-    # Create an interactive violin plot
     fig = px.violin(df, x="day", y="tip")
     fig.show()
