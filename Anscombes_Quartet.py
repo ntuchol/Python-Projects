29b41d886c6b8d69602269032deb9756903fb450
Natalia <ntuchol@bu.edu>
2025-07-24 20:38:33 -0500

    Update Anscombes_Quartet.py

--- Anscombes_Quartet.py
+++ Anscombes_Quartet.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
         data=df,
         x="x",
         y="y",
-        col="dataset",  # Create separate plots for each dataset
-        hue="dataset",  # Color points based on dataset
-        col_wrap=2,     # Wrap plots into two columns
-        ci=None,        # Do not show confidence interval for regression line
+        col="dataset",  
+        hue="dataset",  
+        col_wrap=2,     
+        ci=None,       
         height=4,
-        scatter_kws={"s": 50, "alpha": 1} # Customize scatter plot points
+        scatter_kws={"s": 50, "alpha": 1} 
     )
     plt.suptitle("Anscombe's Quartet", y=1.02) # Add a main title
     plt.show()
