6c9a58d3dda150063a10bc6c8764e1ffc781c717
Natalia <ntuchol@bu.edu>
2025-07-24 22:43:59 -0500

    Update SpamFolder.py

--- SpamFolder.py
+++ SpamFolder.py
@@ -3,7 +3,6 @@ import email
 import os
 import shutil
 
-# Replace with your email credentials and server details
 EMAIL_ADDRESS = "your_email@example.com"
 EMAIL_PASSWORD = "your_password"
 IMAP_SERVER = "imap.example.com"
@@ -12,25 +11,20 @@ DESTINATION_FOLDER = "Trash"
 
 def move_spam_to_trash():
     try:
-        # Connect to the email server
         mail = imaplib.IMAP4_SSL(IMAP_SERVER)
         mail.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         mail.select(SPAM_FOLDER)
 
-        # Search for all messages in the spam folder
         typ, data = mail.search(None, 'ALL')
         for num in data[0].split():
-            # Fetch the message
             typ, msg_data = mail.fetch(num, '(RFC822)')
             for response_part in msg_data:
                 if isinstance(response_part, tuple):
                     msg = email.message_from_bytes(response_part[1])
 
-                    # Move the message to the destination folder
                     mail.copy(num, DESTINATION_FOLDER)
                     mail.store(num, '+FLAGS', '\\Deleted')  # Mark for deletion
 
-        # Expunge (permanently delete) messages marked for deletion
         mail.expunge()
 
         mail.close()
