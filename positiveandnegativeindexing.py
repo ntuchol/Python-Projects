f61fa6e76c7f3fd170c137b797e9036eb4588e6f
Natalia <ntuchol@bu.edu>
2025-07-25 20:50:11 -0500

    Update positiveandnegativeindexing.py

--- positiveandnegativeindexing.py
+++ positiveandnegativeindexing.py
@@ -2,5 +2,5 @@
 
 my_list = [10, 20, 30, 40, 50]
 
-print(my_list[1])   # Output: 20 (positive indexing)
-print(my_list[-4])  # Output: 20 (negative indexing)
+print(my_list[1])  
+print(my_list[-4]) 
