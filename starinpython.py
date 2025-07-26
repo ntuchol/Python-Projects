be2271efbd879d2fb086bf243e750f87d01d8044
Natalia <ntuchol@bu.edu>
2025-07-25 22:59:55 -0500

    Update starinpython.py

--- starinpython.py
+++ starinpython.py
@@ -1,15 +1,11 @@
 import turtle
 
-# Set up the screen
 ws = turtle.Screen()
 
-# Create a turtle instance
 star_turtle = turtle.Turtle()
 
-# Draw a star
 for i in range(5):
 star_turtle.forward(100)
 star_turtle.right(144)
 
-# Finish drawing
 turtle.done()
