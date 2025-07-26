a00a956cb8697bd36411f756777a732d8ccde145
Natalia <ntuchol@bu.edu>
2025-07-25 22:08:30 -0500

    Update reversearray.py

--- reversearray.py
+++ reversearray.py
@@ -1,15 +1,6 @@
 def reverse_array(arr):
-    """Reverses the order of elements in an array.
-
-    Args:
-        arr: The array (list) to be reversed.
-
-    Returns:
-        A new array with elements in reversed order.
-    """
     return arr[::-1]
 
-# Example usage:
 my_array = [1, 2, 3, 4, 5]
 reversed_array = reverse_array(my_array)
-print(reversed_array)  # Output: [5, 4, 3, 2, 1]
+print(reversed_array)  
