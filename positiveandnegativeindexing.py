c65d50155eefd7bc56ad469536779202a3e20c4b
Natalia <ntuchol@bu.edu>
2025-07-25 20:49:56 -0500

    Update positiveandnegativeindexing.py

--- positiveandnegativeindexing.py
+++ positiveandnegativeindexing.py
@@ -2,6 +2,5 @@
 
 my_list = [10, 20, 30, 40, 50]
 
-# Accessing the same element using positive and negative indexing
 print(my_list[1])   # Output: 20 (positive indexing)
 print(my_list[-4])  # Output: 20 (negative indexing)
