ca7fddd7b4eff8271903f1cfdbbc061344cf864e
Natalia <ntuchol@bu.edu>
2025-07-25 20:23:55 -0500

    Update ordered_tree.py

--- ordered_tree.py
+++ ordered_tree.py
@@ -6,4 +6,4 @@ from sortedcontainers import SortedDict
     ordered_map[2] = "b"
 
     for key, value in ordered_map.items():
-        print(key, value)  # Output: (1, 'a'), (2, 'b'), (3, 'c')
+        print(key, value)  
