1ec8001c4696a0de176ecef9b9b22a680f8e30a6
Natalia <ntuchol@bu.edu>
2025-07-25 20:52:20 -0500

    Update postfixexpression.py

--- postfixexpression.py
+++ postfixexpression.py
@@ -1,11 +1,9 @@
 import math
 
-# Function that returns evaluated value of a given postfix expression
 def evaluatePostfix(arr: list[str]) -> int:
     stack = []
 
     for token in arr:
-        # If token is a number, push it onto the stack
         if token.lstrip('-').isdigit():  
             stack.append(int(token))
         else:
