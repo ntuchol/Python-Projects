1f72062104beb8b9a90888b865c910c7db85f0d9
Natalia <ntuchol@bu.edu>
2025-07-25 21:00:10 -0500

    Update pydrawapplication.py

--- pydrawapplication.py
+++ pydrawapplication.py
@@ -16,14 +16,7 @@ def keydown(key):
 def keyup(key):
   print('For when you really just gotta stop moving, keyup is here to save you.')
 
-# All of the above methods must be defined above this statement (because Python):
 screen.listen()
-
-
-# -- As you can see, user input is as simple as defining methods and telling pydraw to listen!
-#    This feature has been long overdue for teachers who want to teach Python to their students
-#    by making really cool stuff but can't escape how ridiculously input has been handled in the past.
-#    Now defining user input is easy to understand and use.
 
 fps = 30
 running = True
