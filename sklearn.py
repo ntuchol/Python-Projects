9e5f2c0702558452fcaba08c1cec320e990cb46a
Natalia <ntuchol@bu.edu>
2025-07-25 22:49:52 -0500

    Update sklearn.py

--- sklearn.py
+++ sklearn.py
@@ -2,18 +2,13 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-# Generate some sample data
 X = np.random.rand(100, 1) * 10
 y = 2 * X + 1 + np.random.randn(100, 1) * 2
 
-# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Instantiate the model
 model = LinearRegression()
 
-# Fit the model to the training data
 model.fit(X_train, y_train)
 
-# Now the model is trained and can be used for predictions
-# predictions = model.predict(X_test)
+predictions = model.predict(X_test)
