c847b971fafc25194f0b3f58a1f8f8b27e5fc157
Natalia <ntuchol@bu.edu>
2025-07-25 23:02:39 -0500

    Update stopsign.py

--- stopsign.py
+++ stopsign.py
@@ -1,21 +1,15 @@
     import sys
     
-    # ... some code ...
-    
-    sys.exit(0)  # Exit with success status
-    # or
-    exit(1) #exit with error status
+
+    exit(1) 
     
     import os
-    
-    # ... some code ...
-    
+        
     os._exit(0)
 
 
 
     def main():
-        # ... some code ...
         return
     
     if __name__ == "__main__":
@@ -27,13 +21,9 @@
     
     def some_process():
         while not stop_flag:
-            # Do something
             print("Running...")
             time.sleep(1)
     
-    # Start the process (e.g., in a separate thread)
-    
-    # To stop it:
     stop_flag = True
     
 
@@ -44,13 +34,12 @@
         raise TimeoutError("Timed out!")
     
     signal.signal(signal.SIGALRM, timeout_handler)
-    signal.alarm(5) # Set timeout for 5 seconds
+    signal.alarm(5) 
     
     try:
-        # Run code that might take too long
         print("Start")
         time.sleep(10)
     except TimeoutError as e:
         print(e)
     finally:
-      signal.alarm(0) # Disable the alarm
+      signal.alarm(0) 
