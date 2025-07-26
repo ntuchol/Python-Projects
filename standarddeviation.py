e692937edbcef04ee22b18542d0708767fe62506
Natalia <ntuchol@bu.edu>
2025-07-25 22:57:57 -0500

    Update standarddeviation.py

--- standarddeviation.py
+++ standarddeviation.py
@@ -3,15 +3,12 @@ import numpy
 
 data = [4, 8, 6, 5, 3, 2, 8, 9, 2, 5]
 
-# Using the statistics module
 std_dev_statistics = statistics.stdev(data)
 print(f"Standard deviation using statistics: {std_dev_statistics}")
 
-# Using the numpy module
 std_dev_numpy = numpy.std(data)
 print(f"Standard deviation using numpy: {std_dev_numpy}")
 
-# Calculating standard deviation manually
 n = len(data)
 mean = sum(data) / n
 deviations = [(x - mean) ** 2 for x in data]
