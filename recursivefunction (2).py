09986744d36d83243f09ba0265dbb433bc279a6a
Natalia <ntuchol@bu.edu>
2025-07-25 22:01:00 -0500

    Update and rename recursivefunction (2).py to factorial_nt.py

--- recursivefunction (2).py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    if n == 0:  # Base case: factorial of 0 is 1
-        return 1
-    else:  # Recursive case: n! = n * (n-1)!
-        return n * factorial(n - 1)
-
-print(factorial(5))  # Output: 120
-    
