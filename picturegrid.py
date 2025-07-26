d5974591e3ba76d1cc18f1da8ab8478bb4350325
Natalia <ntuchol@bu.edu>
2025-07-25 20:36:30 -0500

    Update picturegrid.py

--- picturegrid.py
+++ picturegrid.py
@@ -9,13 +9,12 @@ im3 = np.flipud(im1)
 im4 = np.fliplr(im2)
 
 fig = plt.figure(figsize=(4., 4.))
-grid = ImageGrid(fig, 111,  # similar to subplot(111)
-                 nrows_ncols=(2, 2),  # creates 2x2 grid of Axes
-                 axes_pad=0.1,  # pad between Axes in inch.
+grid = ImageGrid(fig, 111,  
+                 nrows_ncols=(2, 2),  
+                 axes_pad=0.1,  
                  )
 
 for ax, im in zip(grid, [im1, im2, im3, im4]):
-    # Iterating over the grid returns the Axes.
     ax.imshow(im)
 
 plt.show()
