976d0fbc04f07d385f791e7fadff0efbd8c78fe5
Natalia <ntuchol@bu.edu>
2025-07-25 22:57:15 -0500

    Update stackbasedcollection.py

--- stackbasedcollection.py
+++ stackbasedcollection.py
@@ -1,8 +1,8 @@
 from collections import deque
 
 stack = deque()
-stack.append(1) # Push
+stack.append(1) 
 stack.append(2)
 stack.append(3)
-print(stack.pop()) # Pop, Output: 3
-print(stack) # Output: deque([1, 2])
+print(stack.pop()) 
+print(stack) 
