90a54b556b3495bf5dcfa53e8788111ac14f1e53
Natalia <ntuchol@bu.edu>
2025-07-25 23:26:59 -0500

    Update strtime.py

--- strtime.py
+++ strtime.py
@@ -4,5 +4,5 @@ date_string = "2025-06-07 10:30:00"
 format_string = "%Y-%m-%d %H:%M:%S"
 
 datetime_object = datetime.strptime(date_string, format_string)
-print(datetime_object)  # Output: 2025-06-07 10:30:00
-print(type(datetime_object)) # Output: <class 'datetime.datetime'>
+print(datetime_object) 
+print(type(datetime_object)) 
