281511a3a97866d983222c7392ab37fe147dcbd6
Natalia <ntuchol@bu.edu>
2025-07-25 22:53:16 -0500

    Update sortanarray.py

--- sortanarray.py
+++ sortanarray.py
@@ -1,9 +1,7 @@
-# Using the sort() method
 my_array = [5, 2, 8, 1, 9]
 my_array.sort(reverse=True)
-print(my_array)  # Output: [9, 8, 5, 2, 1]
+print(my_array)  
 
-# Using the sorted() function
 my_array = [5, 2, 8, 1, 9]
 new_array = sorted(my_array, reverse=True)
-print(new_array)  # Output: [9, 8, 5, 2, 1]
+print(new_array) 
